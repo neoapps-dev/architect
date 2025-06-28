92548394ff8da90174cbb8bfd12fe37b8af09e13
NEOAPPS <asd22.info@gmail.com>
2025-06-28 17:03:34 +0300

    fix: make it possible to make config if path doesnt exist

--- src/main.sh
+++ src/main.sh
@@ -2,11 +2,13 @@
 set -e
 VERSION=0.0.1-alpha
 CONFIG_FILE="${ARCHITECT_CONFIG:-/etc/architect.sh}"
-if [ ! -f "$CONFIG_FILE" ]; then
+check_config() {
+  [[ -f "$CONFIG_FILE" ]] && source $CONFIG_FILE && return
+  [[ " $* " == *" --make-config "* ]] && return
   echo "[!] Config file not found: $CONFIG_FILE" >&2
   exit 1
-fi
-source $CONFIG_FILE
+}
+check_config "$@"
 DRY_RUN=0
 FORCE=0
 ARCHITECT_DIR="${ARCHITECT_DIR:-$HOME/.architect}"
